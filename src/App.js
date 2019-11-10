c13513e5057a0bb6f5e834d018b4b33392fd6724
tiphaineP1991 <tiphaine.pellet@essec.edu>
2019-11-10 15:02:50 +0100

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [taskInput, setTaskInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const todo = () => {
+    const listItems = [];
+    for (let i = 0; i < tasks.length; i++) {
+      listItems.push(
+        <div className="listItems">
+          <span className="croix">☓</span>
+          <span
+            className={tasks[i].isDone === true ? "item" : "itembarre"}
+            onClick={() => {
+              const newTasks2 = [...tasks];
+              console.log(tasks[i].isDone);
+              newTasks2[i].isDone = !newTasks2[i].isDone;
+              setTasks(newTasks2);
+            }}
+          >
+            {tasks[i].name}
+          </span>
+        </div>
+      );
+    }
+    return listItems;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do list</h1>
+      <div className="list-item"></div>
+      <div>{todo()}</div>
+      <form
+        className="form"
+        onSubmit={event => {
+          event.preventDefault();
+          const newTasks = [...tasks];
+          newTasks.push({ name: taskInput, isDone: true });
+          setTasks(newTasks);
+        }}
+      >
+        <input
+          className="task-input"
+          type="text"
+          name="input"
+          value={taskInput}
+          onChange={event => setTaskInput(event.target.value)}
+        ></input>
+        <input
+          className="submit"
+          type="submit"
+          value="Ajouter une tâche"
+        ></input>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
